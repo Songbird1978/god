8957b6bff3591f67ca8c3dd7415ffc1c9f6089e3
Songbird1978 <118288349+Songbird1978@users.noreply.github.com>
2025-03-05 13:00:22 +0000

    mailing and contact form working but not sending POST yet

--- client/src/components/navbar/navOptions.js
+++ client/src/components/navbar/navOptions.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Link from 'react';
+import { Link } from 'react-router-dom';
 
 
 function NavOptions() {
@@ -29,11 +29,12 @@ function NavOptions() {
                         window.location.replace("/#listen");  // Perform the redirect
                     }} className="columnOneNav" style={{ textDecoration: 'none', cursor: "pointer" }}  >LISTEN</div>
                 </li>
-
                 <li  >
-                    <Link to="https://www.ebay.co.uk/itm/335571305995?_skw=gary+o+donnell+three+ways&itmmeta=01JNHSFWK62979022Q6T7BX86B&hash=item4e219bc60b:g:AGYAAOSwZgBm3Bi4&itmprp=enc%3AAQAKAAAA0FkggFvd1GGDu0w3yXCmi1d4%2F7uN5Qku0U1yYHFJls01IunjbzsDuDBgTw7PlSKQeHiqpeuQAVq9efRy7LZw4xm1ueXAH8hhtQrC1t7SdpnLLdMjVSER5TQfSmrw4Dc%2BJGWizKWBuxoANzWGJ1ErHq8i04arCro9E%2Br9wR3fcij9o8BCKyyU9xhnfhWyWChoTDKsEY4OXK57XB%2BPXaGLWy%2BAMqrIgwrbAdVYdOpz11YqqaG73JV2uldQX72gjYwH9L%2BZpIAamAaLc83rzMNqGXY%3D%7Ctkp%3ABk9SR-DJv7msZQ" className="columnOneNav">
-                        <div className="columnOneNav" to="#store" style={{ textDecoration: 'none', cursor: "pointer" }} >STORE</div>
-                    </Link>
+                    <div className="columnOneNav">
+                        <Link to="https://www.ebay.co.uk/itm/335571305995" style={{ textDecoration: 'none', cursor: "pointer", color: '#403434' }}>
+                            STORE
+                        </Link>
+                    </div>
                 </li>
             </ul>
         </>
