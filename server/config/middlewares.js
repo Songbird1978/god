87d0d1e4594f8dc11b0cdd8937c1547b1f93bf4c
Songbird1978 <118288349+Songbird1978@users.noreply.github.com>
2025-12-06 14:21:30 +0000

    added render to middlewares

--- server/config/middlewares.js
+++ server/config/middlewares.js
@@ -1,26 +1,27 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      origin: [
-        'http://localhost:3000',
-        'https://god-sage.vercel.app',
-        'https://garyodandthedsh.com',
-        'https://www.garyodandthedsh.com'
-      ],
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-      credentials: true,
+    "strapi::errors",
+    "strapi::security",
+    {
+        name: "strapi::cors",
+        config: {
+            enabled: true,
+            origin: [
+                "http://localhost:3000",
+                "https://god-6usq.onrender.com",
+                "https://god-sage.vercel.app",
+                "https://garyodandthedsh.com",
+                "https://www.garyodandthedsh.com",
+            ],
+            methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+            headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+            credentials: true,
+        },
     },
-  },
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+    "strapi::poweredBy",
+    "strapi::logger",
+    "strapi::query",
+    "strapi::body",
+    "strapi::session",
+    "strapi::favicon",
+    "strapi::public",
 ];
