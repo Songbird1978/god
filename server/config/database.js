e06745fb7c5a0dd092434a3136f267a46d8274d4
Hayley Callender <118288349+Songbird1978@users.noreply.github.comgit>
2025-12-06 15:44:36 +0000

    up to date with laptop

--- server/config/database.js
+++ server/config/database.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => {
     const connections = {
         sqlite: {
             connection: {
-                filename: env("DATABASE_FILENAME"),
+                filename: env("DATABASE_FILENAME") || "/data/data.db",
             },
             useNullAsDefault: true,
         },
