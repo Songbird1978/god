dd3078d37c90ddb866365f1751921284ecf76104
Songbird1978 <118288349+Songbird1978@users.noreply.github.com>
2025-12-06 07:57:22 +0000

    altered config files for var/data/

--- server/config/database.js
+++ server/config/database.js
@@ -1,40 +1,47 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+    const client = env("DATABASE_CLIENT", "sqlite");
 
-  const connections = {
-    mysql: {
-      connection: {
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+    const connections = {
+        mysql: {
+            connection: {
+                host: env("DATABASE_HOST", "localhost"),
+                port: env.int("DATABASE_PORT", 3306),
+                database: env("DATABASE_NAME", "strapi"),
+                user: env("DATABASE_USERNAME", "strapi"),
+                password: env("DATABASE_PASSWORD", "strapi"),
+                ssl: env.bool("DATABASE_SSL", false) && {
+                    key: env("DATABASE_SSL_KEY", undefined),
+                    cert: env("DATABASE_SSL_CERT", undefined),
+                    ca: env("DATABASE_SSL_CA", undefined),
+                    capath: env("DATABASE_SSL_CAPATH", undefined),
+                    cipher: env("DATABASE_SSL_CIPHER", undefined),
+                    rejectUnauthorized: env.bool(
+                        "DATABASE_SSL_REJECT_UNAUTHORIZED",
+                        true
+                    ),
+                },
+            },
+            pool: { min: 2, max: 10 },
         },
-      },
-      pool: { min: 2, max: 10 },
-    },
-    sqlite: {
-      connection: {
-        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '/data/data.db')),
-      },
-      useNullAsDefault: true,
-    },
-  };
+        sqlite: {
+            connection: {
+                filename: path.join(
+                    __dirname,
+                    "..",
+                    env("DATABASE_FILENAME", "/var/data/data.db")
+                ),
+            },
+            useNullAsDefault: true,
+        },
+    };
 
-  return {
-    connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: 60000,
-    },
-  };
+    return {
+        connection: {
+            client,
+            ...connections[client],
+            acquireConnectionTimeout: 60000,
+        },
+    };
 };
