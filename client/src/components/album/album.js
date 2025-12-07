3383c74de7d4a332e94328018414076684cacded
Hayley Callender <118288349+Songbird1978@users.noreply.github.comgit>
2025-12-07 09:19:29 +0000

    updated colours, spacing, added grid for blogs for alignment

--- client/src/components/album/album.js
+++ client/src/components/album/album.js
@@ -12,21 +12,23 @@ function Album(hero) {
 
             </div>
 
-            <div className='buttonsArea'>
-                <div className="albumListdiv, actionTitle" id="albumStatement">
-                    NEW ALBUM AVAILABLE NOW</div >
-                <a href="https://garyodonnellthedimestoreheroes.bandcamp.com/album/three-ways-from-nowhere" target="_blank"
-                    rel="noopener noreferrer" className="albumListdiv">
-                    <div className="button">BUY </div>
-                </a>
-                <div className="albumListdiv" style={{ paddingBottom: '40px'}}>
-                    <div onClick={() => window.location.replace("/#listen")}
-                        className="button" style={{ scrollBehaviour: "smooth" }}>LISTEN</div>
-                </div>
-                <div style={{display: 'flex', width: '100vw', height: '80vh', backgroundColor: '#4A5375', padding: "20px", alignItems: 'center'}}>
+                    <div className='buttonsArea'>
+                        <div className="albumListdiv, actionTitle" id="albumStatement">
+                            NEW ALBUM AVAILABLE NOW</div >
+                        <a href="https://garyodonnellthedimestoreheroes.bandcamp.com/album/three-ways-from-nowhere" target="_blank"
+                            rel="noopener noreferrer" className="albumListdiv">
+                            <div className="button">BUY </div>
+                        </a>
+                        <div className="albumListdiv" style={{ paddingBottom: '40px'}}>
+                            <div onClick={() => window.location.replace("/#listen")}
+                                className="button" style={{ scrollBehaviour: "smooth" }}>LISTEN</div>
+                        </div>
+                        
+                    </div>
+                    <div style={{display: 'flex', flexDirection: "column", width: '100vw', height: '100vh', backgroundColor: '#4A5375', padding: "40px", alignItems: 'center', justifyContent: 'center'}}>
                     <img src="https://res.cloudinary.com/djtvlgplj/image/upload/v1765044989/gary_at_the_wedge_dad7c2de6e.jpg" alt="Gary at the Wedgewood Rooms" style={{width: '20rem', height: '20rem'}}/>
                     <div style={{ display: "block", justifyContent: "center", padding: '20px'}}>
-                        <div className="albumListdiv, actionTitle" id="albumStatement" style={{}}>
+                        <div className="albumListdiv, actionTitle" id="showsStatement" style={{}}>
                         LIVE SHOWS COMING SOON...
                         </div >
                         <div>
@@ -43,7 +45,6 @@ function Album(hero) {
                             </div>
                     </div>
                 </div>
-            </div>
                 </div>
             </div>
         </div>
